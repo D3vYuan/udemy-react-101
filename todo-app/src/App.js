8d3e02e7c2d4eb316d48c0d5a32ca8756399704f
TheMaos <yuanjian@hotmail.com>
2023-12-11 16:11:38 +0800

    lesson 17 - create second component

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
-import ToDoRowItem from "./components/TodoRowItem";
-
+import TodoTable from "./components/TodoTable";
 function App() {
   const todos = [
     { rowNumber: 1, rowDescription: "Feed Puppy", rowAssigned: "User One" },
@@ -13,32 +12,7 @@ function App() {
       <div className="card">
         <div className="card-header">Your Todo's</div>
         <div className="card-body">
-          <table className="table table-hover">
-            <thead>
-              <tr>
-                <th scope="col">#</th>
-                <th scope="col">Description</th>
-                <th scope="col">Assigned</th>
-              </tr>
-            </thead>
-            <tbody>
-              <ToDoRowItem
-                rowNumber={todos[0].rowNumber}
-                rowDescription={todos[0].rowDescription}
-                rowAssigned={todos[0].rowAssigned}
-              />
-              <ToDoRowItem
-                rowNumber={todos[1].rowNumber}
-                rowDescription={todos[1].rowDescription}
-                rowAssigned={todos[1].rowAssigned}
-              />
-              <ToDoRowItem
-                rowNumber={todos[2].rowNumber}
-                rowDescription={todos[2].rowDescription}
-                rowAssigned={todos[2].rowAssigned}
-              />
-            </tbody>
-          </table>
+          <TodoTable todos={todos} />
         </div>
       </div>
     </div>
