a40c29284732fd0ba3c410948f4e611448c90e2e
TheMaos <yuanjian@hotmail.com>
2023-12-20 15:58:47 +0800

    lesson 19 - onclick listeners

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -12,12 +12,27 @@ function App() {
     },
   ];
 
+  const addTodo = () => {
+    if (todos.length > 0) {
+      const newTodo = {
+        rowNumber: todos.length + 1,
+        rowDescription: "New Todo",
+        rowAssigned: "User Three",
+      };
+      todos.push(newTodo);
+      console.log(todos);
+    }
+  };
+
   return (
     <div className="mt-5 container">
       <div className="card">
         <div className="card-header">Your Todo's</div>
         <div className="card-body">
           <TodoTable todos={todos} />
+          <button className="btn btn-primary" onClick={addTodo}>
+            Add New Todo
+          </button>
         </div>
       </div>
     </div>
