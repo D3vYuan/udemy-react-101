33517f238a39dbf0e9149f39c82e90499ddc5303
TheMaos <yuanjian@hotmail.com>
2023-12-21 16:01:17 +0800

    lesson 58 - init library app

---
+++ library-frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Welcome to the Mao Mao Shop</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ library-frontend/start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# npx create-react-app todo-app
+# npm install
+# set PORT=8080 && npm start
+npm start
