b5966b6f7b96c85a7b3348a6edd54b7d7bde3bc8
TheMaos <yuanjian@hotmail.com>
2023-12-21 14:53:20 +0800

    lesson 25 - delete item using react function

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -30,12 +30,19 @@ function App() {
     setTodos((todos) => [...todos, newTodo]);
   };
 
+  const deleteTodo = (deleteTodoRowNumber) => {
+    let filtered = todos.filter(function (value) {
+      return value.rowNumber !== deleteTodoRowNumber;
+    });
+    setTodos(filtered);
+  };
+
   return (
     <div className="mt-5 container">
       <div className="card">
         <div className="card-header">Your Todo's</div>
         <div className="card-body">
-          <TodoTable todos={todos} />
+          <TodoTable todos={todos} deleteTodo={deleteTodo} />
           <button className="btn btn-primary">Add New Todo</button>
           <NewTodoForm addTodo={addTodo} />
         </div>
