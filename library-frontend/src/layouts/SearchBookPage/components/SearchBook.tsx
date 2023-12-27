818350162e5e5b0f90c70c5bd64c6ac9cef9f930
TheMaos <yuanjian@hotmail.com>
2023-12-27 15:47:39 +0800

    lesson 90 - search book page component

--- library-frontend/src/layouts/SearchBookPage/components/SearchBook.tsx
+++ library-frontend/src/layouts/SearchBookPage/components/SearchBook.tsx
@@ -19,15 +19,29 @@ export const SearchBook: React.FC<{ book: BookModel }> = (props) => {
           </div>
         </div>
 
+        {/* Mobile Version */}
+        <div className="d-lg-none d-flex justify-content-center align-items-center">
+          {props.book.img ? (
+            <img src={props.book.img} width="123" height="196" alt="Book" />
+          ) : (
+            <img
+              src={require("../../../Images/BooksImages/book-luv2code-1000.png")}
+              width="123"
+              height="196"
+              alt="Book"
+            />
+          )}
+        </div>
+
         <div className="col-md-6">
           <div className="card-body">
             <h5 className="card-title">{props.book.author}</h5>
-            <h4>{props.book.description}</h4>
+            <h4>{props.book.title}</h4>
             <p className="card-text">{props.book.description}</p>
           </div>
         </div>
 
-        <div className="col-md-4 d-flex justify-content-center align-content-center">
+        <div className="col-md-4 d-flex justify-content-center align-items-center">
           <a className="btn btn-md main-color text-white" href="#">
             View Details
           </a>
