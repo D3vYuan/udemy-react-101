169eb3cdae529b779dc7d4aa9acc313a64f6f59d
TheMaos <yuanjian@hotmail.com>
2023-12-27 14:47:00 +0800

    lesson84 - home page return books restful call

--- library-frontend/src/layouts/HomePage/components/Carousel.tsx
+++ library-frontend/src/layouts/HomePage/components/Carousel.tsx
@@ -50,12 +50,12 @@ export const Carousel = () => {
     );
   }
 
-  if (httpError){
+  if (httpError) {
     return (
-        <div className="container m-5">
-            <p>{httpError}</p>
-        </div>
-    )
+      <div className="container m-5">
+        <p>{httpError}</p>
+      </div>
+    );
   }
 
   return (
@@ -73,23 +73,25 @@ export const Carousel = () => {
         <div className="carousel-inner">
           <div className="carousel-item active">
             <div className="row d-flex justify-content-center  align-content-center">
-              <ReturnBook />
-              <ReturnBook />
-              <ReturnBook />
+              {books.slice(0, 3).map((book) => (
+                <ReturnBook book={book} key={book.id} />
+              ))}
             </div>
           </div>
 
           <div className="carousel-item">
             <div className="row d-flex justify-content-center  align-content-center">
-              <ReturnBook />
-              <ReturnBook />
-              <ReturnBook />
+              {books.slice(3, 6).map((book) => (
+                <ReturnBook book={book} key={book.id} />
+              ))}
             </div>
           </div>
 
           <div className="carousel-item">
             <div className="row d-flex justify-content-center  align-content-center">
-              <ReturnBook />
+              {books.slice(6, 9).map((book) => (
+                <ReturnBook book={book} key={book.id} />
+              ))}
             </div>
           </div>
         </div>
@@ -124,7 +126,7 @@ export const Carousel = () => {
       {/* Mobile */}
       <div className="d-lg-none mt-3">
         <div className="row d-flex justify-content-center  align-items-center">
-          <ReturnBook />
+          <ReturnBook book={books[0]} key={books[0].id} />
         </div>
       </div>
 
