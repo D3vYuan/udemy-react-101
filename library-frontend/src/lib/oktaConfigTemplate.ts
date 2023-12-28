6bdb52a4e5e848c4d0c540a80053ed5edc7ef01d
TheMaos <yuanjian@hotmail.com>
2023-12-28 23:46:30 +0800

    lesson 128 - added okta configuration

---
+++ library-frontend/src/lib/oktaConfigTemplate.ts
@@ -0,0 +1,10 @@
+// 1. Replace <okta-client-id> and <okta-client-user>
+// 2. Rename oktaConfigTemplate to oktaConfig
+export const oktaConfigTemplate = {
+        clientId: "<okta-client-id>",
+        issuer: "https://<okta-client-user>/oauth2/default",
+        redirectUri: "http://localhost:3000/login/callback",
+        scopes: ['openid', 'profile', 'email'],
+        pkce: true,
+        disableHttpsCheck: true
+}
