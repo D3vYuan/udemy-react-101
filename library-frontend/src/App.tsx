675ce77dd9f312f98ed141468696db8466010901
TheMaos <yuanjian@hotmail.com>
2023-12-28 14:23:50 +0800

    lesson 103 - react router switch and redirect

--- library-frontend/src/App.tsx
+++ library-frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { Route } from "react-router-dom";
+import { Redirect, Route, Switch } from "react-router-dom";
 import "./App.css";
 import { HomePage } from "./layouts/HomePage/HomePage";
 import { Footer } from "./layouts/NavbarAndFooter/Footer";
@@ -9,12 +9,18 @@ export const App = () => {
   return (
     <div>
       <Navbar />
-      <Route path="/">
-        <HomePage />
-      </Route>
-      <Route path="/search">
-        <SearchBookPage />
-      </Route>
+      <Switch>
+        {/* exact so that the / would not affect other paths */}
+        <Route path="/" exact>
+          <Redirect to="/home" />
+        </Route>
+        <Route path="/home">
+          <HomePage />
+        </Route>
+        <Route path="/search">
+          <SearchBookPage />
+        </Route>
+      </Switch>
       <Footer />
     </div>
   );
