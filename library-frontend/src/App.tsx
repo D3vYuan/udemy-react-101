259da788a1df14628dbbd5ab521c34502861637c
TheMaos <yuanjian@hotmail.com>
2023-12-29 00:07:31 +0800

    lesson 132 - okta configuration in app

--- library-frontend/src/App.tsx
+++ library-frontend/src/App.tsx
@@ -1,12 +1,26 @@
-import { Redirect, Route, Switch } from "react-router-dom";
+import { OktaAuth, toRelativeUrl } from "@okta/okta-auth-js";
+import { Redirect, Route, Switch, useHistory } from "react-router-dom";
 import "./App.css";
 import { BookCheckoutPage } from "./layouts/BookCheckoutPage/BookCheckoutPage";
 import { HomePage } from "./layouts/HomePage/HomePage";
 import { Footer } from "./layouts/NavbarAndFooter/Footer";
 import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
 import { SearchBookPage } from "./layouts/SearchBookPage/SearchBookPage";
+import { oktaConfig } from "./lib/oktaConfig";
+
+const oktaAuth = new OktaAuth(oktaConfig);
 
 export const App = () => {
+  const customAuthHandler = () => {
+    history.push("/login");
+  };
+
+  const history = useHistory();
+
+  const restoreOriginalUri = async (_oktaAuth: any, originalUri: any) => {
+    history.replace(toRelativeUrl(originalUri || "/", window.location.origin));
+  };
+
   return (
     <div className="d-flex flex-column min-vh-100">
       <Navbar />
