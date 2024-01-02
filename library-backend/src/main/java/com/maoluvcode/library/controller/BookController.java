5df96a5ba0090c9b2a5ee1656b1515504246d042
TheMaos <yuanjian@hotmail.com>
2024-01-02 08:58:34 +0800

    lesson 140 - added book checkout repository

---
+++ library-backend/src/main/java/com/maoluvcode/library/controller/BookController.java
@@ -0,0 +1,28 @@
+package com.maoluvcode.library.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.maoluvcode.library.entity.Book;
+import com.maoluvcode.library.service.BookService;
+
+@CrossOrigin("http://localhost:3000")
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+    private BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @PutMapping("/secure/checkout")
+    public Book checkoutBook(@RequestParam Long bookId) throws Exception {
+        String userEmail = "testuser@email.com";
+        return bookService.checkoutBook(userEmail, bookId);
+    }
+
+}
