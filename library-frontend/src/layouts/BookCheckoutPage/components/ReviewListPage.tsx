d00ecd741209fc095976f1628cc37cd0823ff196
TheMaos <yuanjian@hotmail.com>
2024-01-02 17:30:28 +0800

    lesson 174 - review list page pagination

--- library-frontend/src/layouts/BookCheckoutPage/components/ReviewListPage.tsx
+++ library-frontend/src/layouts/BookCheckoutPage/components/ReviewListPage.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import ReviewModel from "../../../models/ReviewModel";
+import { Pagination } from "../../Utils/Pagination";
+import { Review } from "../../Utils/Review";
 import { SpinnerLoading } from "../../Utils/SpinnerLoading";
 
 export const ReviewListPage = () => {
@@ -11,7 +13,7 @@ export const ReviewListPage = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [reviewsPerPage] = useState(5);
   const [totalAmountOfReviews, setTotalAmountOfReviews] = useState(0);
-  const [totalPage, setTotalPages] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
 
   const bookId = window.location.pathname.split("/")[2];
 
@@ -75,5 +77,27 @@ export const ReviewListPage = () => {
 
   const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
 
-  return <div>asd</div>;
+  return (
+    <div className="container m-5">
+      <div>
+        <h3>Comments: ({reviews.length})</h3>
+      </div>
+      <p>
+        {indexOfFirstReview + 1} to {lastItem} of {totalAmountOfReviews} items:
+      </p>
+      <div className="row">
+        {reviews.map((review) => (
+          <Review review={review} key={review.id} />
+        ))}
+      </div>
+
+      {totalPages > 1 && (
+        <Pagination
+          currentPage={currentPage}
+          totalPages={totalPages}
+          paginate={paginate}
+        />
+      )}
+    </div>
+  );
 };
