5fa4cf7180dffa816642107358ab5628972ecb69
TheMaos <yuanjian@hotmail.com>
2024-01-02 11:39:56 +0800

    leson 151 - loan count useEffect

--- library-frontend/src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
+++ library-frontend/src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -1,3 +1,4 @@
+import { useOktaAuth } from "@okta/okta-react";
 import { useEffect, useState } from "react";
 import BookModel from "../../models/BookModel";
 import ReviewModel from "../../models/ReviewModel";
@@ -7,6 +8,8 @@ import { CheckoutAndReviewBox } from "./components/CheckoutAndReviewBox";
 import { LatestReviews } from "./components/LatestReviews";
 
 export const BookCheckoutPage = () => {
+  const { authState } = useOktaAuth;
+
   const [book, setBook] = useState<BookModel>();
   const [isLoading, setIsLoading] = useState(true);
   const [httpError, setHttpError] = useState(null);
@@ -15,6 +18,11 @@ export const BookCheckoutPage = () => {
   const [reviews, setReviews] = useState<ReviewModel[]>([]);
   const [totalStars, setTotalStars] = useState(0);
   const [isLoadingReview, setIsLoadingReview] = useState(true);
+
+  // Loans Count State
+  const [currentLoansCount, setCurrentLoansCount] = useState(0);
+  const [isLoadingCurrentLoanCount, setIsLoadingCurrentLoanCount] =
+    useState(true);
 
   const bookId = window.location.pathname.split("/")[2];
 
@@ -90,6 +98,14 @@ export const BookCheckoutPage = () => {
       setHttpError(error.message);
     });
   }, []); // If state inside array changes will trigger the useEffect
+
+  useEffect(() => {
+    const fetchUserCurrentLoansCount = async () => {};
+    fetchUserCurrentLoansCount().catch((error: any) => {
+      setIsLoadingCurrentLoanCount(false);
+      setHttpError(error.message);
+    });
+  }, [authState]);
 
   if (isLoading || isLoadingReview) {
     return (
