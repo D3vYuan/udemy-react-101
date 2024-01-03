230426187f7904ee0f995462ff0424fa847cb877
TheMaos <yuanjian@hotmail.com>
2024-01-03 15:08:36 +0800

    lesson 206 - message entity

---
+++ library-backend/src/main/java/com/maoluvcode/library/entity/Message.java
@@ -0,0 +1,53 @@
+package com.maoluvcode.library.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "messages")
+@Data
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "user_email")
+    private String userEmail;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "question")
+    private String question;
+
+    @Column(name = "admin_email")
+    private String adminEmail;
+
+    @Column(name = "response")
+    private String response;
+
+    @Column(name = "closed")
+    private Boolean closed;
+
+    public Message() {
+    }
+
+    public Message(String userEmail, String title, String question, String adminEmail, String response,
+            Boolean closed) {
+        this.userEmail = userEmail;
+        this.title = title;
+        this.question = question;
+        this.adminEmail = adminEmail;
+        this.response = response;
+        this.closed = closed;
+    }
+
+}
