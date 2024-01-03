ec3c3648390e945a101c38cab14dd2958a916ff2
TheMaos <yuanjian@hotmail.com>
2024-01-03 16:30:58 +0800

    lesson 217 - message useEffect

---
+++ library-frontend/src/layouts/MessagesPage/components/Message.tsx
@@ -0,0 +1,73 @@
+import { useOktaAuth } from "@okta/okta-react";
+import { useEffect, useState } from "react";
+import MessageModel from "../../../models/MessageModel";
+import { SpinnerLoading } from "../../Utils/SpinnerLoading";
+
+export const Messages = () => {
+  const { authState } = useOktaAuth();
+  const [isLoadingMessages, setIsLoadingMessages] = useState(true);
+  const [httpError, setHttpError] = useState(null);
+
+  // Messages
+  const [messages, setMessages] = useState<MessageModel[]>([]);
+
+  // Pagination
+  const [messagesPerPage] = useState(5);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+
+  useEffect(() => {
+    const fetchUserMessages = async () => {
+      if (authState && authState.isAuthenticated) {
+        const url = `http://localhost:8080/api/messages/search/findByUserEamil?userEmail=${
+          authState?.accessToken?.claims.sub
+        }&page=${currentPage - 1}&size=${messagesPerPage}`;
+        const requestOptions = {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${authState.accessToken?.accessToken}`,
+            "Content-Type": "application/json",
+          },
+        };
+        const messagesResponse = await fetch(url, requestOptions);
+        if (!messagesResponse.ok) {
+          throw new Error("Something went wrong!");
+        }
+
+        const messagesResponseJson = await messagesResponse.json();
+        setMessages(messagesResponseJson._embedded.messages);
+        setTotalPages(messagesResponseJson.page.totalPages);
+      }
+      setIsLoadingMessages(false);
+    };
+    fetchUserMessages().catch((error: any) => {
+      setIsLoadingMessages(false);
+      setHttpError(error.messages);
+    });
+    window.scrollTo(0, 0);
+  }, [authState, currentPage]);
+
+  if (isLoadingMessages) {
+    return (
+      <div className="container m-5">
+        <SpinnerLoading />
+      </div>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <div className="container m-5">
+        <p>{httpError}</p>
+      </div>
+    );
+  }
+
+  const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+};
