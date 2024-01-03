b2f28f284d6ca12560796470d3a92252272bc945
TheMaos <yuanjian@hotmail.com>
2024-01-04 00:59:48 +0800

    lesson 233 - message controller put message

--- library-backend/src/main/java/com/maoluvcode/library/controller/MessageController.java
+++ library-backend/src/main/java/com/maoluvcode/library/controller/MessageController.java
@@ -2,11 +2,13 @@ package com.maoluvcode.library.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.maoluvcode.library.dto.AdminQuestionRequest;
 import com.maoluvcode.library.entity.Message;
 import com.maoluvcode.library.service.MessageService;
 import com.maoluvcode.library.utils.ExtractJWT;
@@ -18,6 +20,7 @@ public class MessageController {
 
     private MessageService messageService;
     private static final String SUB_FIELD = "\"sub\"";
+    private static final String USER_TYPE_FIELD = "\"userType\"";
 
     public MessageController(MessageService messageService) {
         this.messageService = messageService;
@@ -29,4 +32,15 @@ public class MessageController {
         messageService.postMessage(messageRequest, userEmail);
     }
 
+    @PutMapping("/secure/admin/message")
+    public void putMessage(@RequestHeader("Authorization") String token,
+            @RequestBody AdminQuestionRequest adminQuestionRequest) throws Exception {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, SUB_FIELD);
+        String userType = ExtractJWT.payloadJWTExtraction(token, USER_TYPE_FIELD);
+
+        if (userType == null || !userType.equals("admin")) {
+            throw new Exception("Administration page only.");
+        }
+        messageService.putMessage(adminQuestionRequest, userEmail);
+    }
 }
