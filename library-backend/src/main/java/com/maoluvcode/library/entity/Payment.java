ab616f91a64a4794966750e4dc7e189ae18fcda0
TheMaos <yuanjian@hotmail.com>
2024-01-04 17:24:05 +0800

    lesson 279 - payment entity

---
+++ library-backend/src/main/java/com/maoluvcode/library/entity/Payment.java
@@ -0,0 +1,26 @@
+package com.maoluvcode.library.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "payment")
+@Data
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "user_email")
+    private String userEmail;
+
+    @Column(name = "amount")
+    private double amount;
+}
