8cf1d25803617fc49dceec5754e2eaa2c0d1887b
TheMaos <yuanjian@hotmail.com>
2024-01-05 16:10:48 +0800

    lesson 294 - create payment page

--- library-frontend/src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
+++ library-frontend/src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -32,6 +32,9 @@ export const BookCheckoutPage = () => {
   const [isCheckedOut, setIsCheckedOut] = useState(false);
   const [isLoadingBookCheckedOut, setIsLoadingBookCheckedOut] = useState(true);
 
+  // Payment
+  const [displayError, setDisplayError] = useState(false);
+
   const bookId = window.location.pathname.split("/")[2];
 
   //   Fetch Book
@@ -227,9 +230,10 @@ export const BookCheckoutPage = () => {
     };
     const checkoutResponse = await fetch(url, requestOptions);
     if (!checkoutResponse.ok) {
+      setDisplayError(true);
       throw new Error("Something went wrong!");
     }
-
+    setDisplayError(false);
     setIsCheckedOut(true);
   }
 
@@ -263,7 +267,13 @@ export const BookCheckoutPage = () => {
 
   return (
     <div>
+      {/* Desktop */}
       <div className="container d-none d-lg-block">
+        {displayError && (
+          <div className="alert alert-danger mt-3" role="alert">
+            Please pay outstanding fees and/or return late book(s).
+          </div>
+        )}
         <div className="row mt-5">
           <div className="col-sm-2 col-md-2">
             {book?.img ? (
@@ -303,6 +313,11 @@ export const BookCheckoutPage = () => {
 
       {/* Mobile Version */}
       <div className="container d-lg-none mt-5">
+        {displayError && (
+          <div className="alert alert-danger mt-3" role="alert">
+            Please pay outstanding fees and/or return late book(s).
+          </div>
+        )}
         <div className="d-flex justify-content-center  align-items-center">
           {book?.img ? (
             <img src={book?.img} width="226" height="349"></img>
